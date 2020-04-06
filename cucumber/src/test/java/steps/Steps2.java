2ec6de34e637c2fc5bc444a721afe2ca6466c9c3
shovalsolo <shovalso@gmail.com>
2020-04-06 15:05:51 -0400

    Adding Examples: to run the test several times

--- cucumber/src/test/java/steps/Steps2.java
+++ cucumber/src/test/java/steps/Steps2.java
@@ -1,8 +1,10 @@
 /**
  * @author Solomon
- * This is the code to file "login.feature"
+ * This is the code to file "login.feature2" this will run the test twice , once for each user
  * step definition class
  * To import all the missing imports click on Ctrl + shift + o
+ * New user created qatest 
+ * Password qatest1
  */
 package steps;
 
@@ -19,26 +21,25 @@ import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
-public class Steps {
+public class Steps2 {
 	
 	private WebDriver driver;
 	
-	@Given("I am in the login page")
-	@Given("I am in the login page of the Para Bank Application")
-	public void i_am_in_the_login_page_of_the_Para_Bank_Application() {
+	@Given("I am in the login page of the Bank Application")
+	public void i_am_in_the_login_page_of_the_Para_Application() {
 	    
 		WebDriverManager.chromedriver().setup();							//Using WebDriverManager after adding to POM.xml
 		driver = new ChromeDriver();
 		driver.get("http://parabank.parasoft.com/parabank/index.htm");
 	}
 
-	@When("I enter valid credentials")
-	public void i_enter_valid_credentials() {
+	@When("I enter the valid {string} and {string}")
+	public void i_enter_the_valid_credentials(String username, String password) {
 	   
 		try {
 			Thread.sleep(3000);
-			driver.findElement(By.name("username")).sendKeys("qatest");
-			driver.findElement(By.name("password")).sendKeys("qatest1");
+			driver.findElement(By.name("username")).sendKeys(username);
+			driver.findElement(By.name("password")).sendKeys(password);
 			driver.findElement(By.xpath("//input[@class='button']")).click();
 		} 
 		catch (InterruptedException e) {
@@ -47,8 +48,8 @@ public class Steps {
 		}
 	}
 
-	@Then("I should be taken to the Overview page")
-	public void i_should_be_taken_to_the_Overview_page() {
+	@Then("I should be taken to the Overview page screen")
+	public void i_should_be_taken_to_the_Overview_page_screen() {
 		
 		WebDriverWait wait = new WebDriverWait(driver, 20);
 		
